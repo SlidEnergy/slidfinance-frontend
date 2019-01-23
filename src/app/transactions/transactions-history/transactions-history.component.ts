15f7403554b864e777f11ac680b69672701e6c33
kluchko <kluchko@lers.ru>
2019-01-23 13:55:09 +1000

    add filter transaction

--- src/app/transactions/transactions-history/transactions-history.component.ts
+++ src/app/transactions/transactions-history/transactions-history.component.ts
@@ -42,29 +42,53 @@ export class TransactionsHistoryComponent implements OnInit {
     this.categoriesService.getCategory().pipe(map(x => new Map(x.map(i => [i.id, i] as [string, Category])))).subscribe(data => this.categories = data);
     this.accountsService.getAccounts().pipe(map(x => new Map(x.map(i => [i.id, i] as [string, Account])))).subscribe(data => this.accounts = data);
     this.dataSource.sort = this.sort;
-    this.dataSource.sortingDataAccessor = (item, property) => {
-      switch (property) {
-        case 'category': {
-          if (!item.categoryId)
-            return '';
-
-          let category = this.categories.get(item.categoryId);
-          return category ? category.title : '';
-        }
-        case 'account': {
-          if (!item.accountId)
-            return '';
-
-          let account = this.accounts.get(item.accountId);
-          return account ? account.title : '';
-        }
-
-        default: return item[property];
-      }
-    };
+    this.dataSource.sortingDataAccessor = this.sortingDataAccessor.bind(this);
+    this.dataSource.filterPredicate = this.filterPredicate.bind(this);
   }
 
-  category_Changed(e: any, transaction: Transaction) {
+  sortingDataAccessor(transaction, property) {
+    switch (property) {
+      case 'category': {
+        if (!transaction.categoryId)
+          return '';
+
+        let category = this.categories.get(transaction.categoryId);
+        return category ? category.title : '';
+      }
+      case 'account': {
+        if (!transaction.accountId)
+          return '';
+
+        let account = this.accounts.get(transaction.accountId);
+        return account ? account.title : '';
+      }
+
+      default: return transaction[property];
+    }
+  }
+
+  filterPredicate(transaction, filter) {
+    if (!filter)
+      return true;
+
+
+    if (!filter || JSON.stringify(transaction).toLowerCase().indexOf(filter) >= 0)
+      return true;
+
+    let data = "";
+
+    let account = this.accounts.get(transaction.accountId);
+    data += account ? account.title : "";
+    let category = this.categories.get(transaction.categoryId);
+    data += category ? category.title : "";
+
+    if (data.toLowerCase().indexOf(filter) >= 0)
+      return true;
+
+    return false;
+  }
+
+  category_Changed(transaction: Transaction) {
     this.transactionsService.patchTransaction(transaction.id, { categoryId: transaction.categoryId })
       .subscribe(() => {
         this.snackBar.open('Категория изменена', undefined, { duration: 5000, panelClass: ['background-green'] });
@@ -83,5 +107,9 @@ export class TransactionsHistoryComponent implements OnInit {
 
   getCategoriesArray() {
     return this.categories && Array.from(this.categories.values());
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
