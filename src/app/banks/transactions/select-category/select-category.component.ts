e15cf01646790aa48e4e478e1219fcd676b40d4e
SlidEnergy <slidenergy@gmail.com>
2019-03-02 21:04:05 +1000

    chore: сделал вывод категорий при вводе транзакции в один столбец, теперь категории берутся из хранилища

--- src/app/banks/transactions/select-category/select-category.component.ts
+++ src/app/banks/transactions/select-category/select-category.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Category, CategoriesService } from 'src/app/api';
 import { Observable } from 'rxjs';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/shared/app-state';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-select-category',
@@ -11,12 +14,13 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class SelectCategoryComponent implements OnInit {
   categories: Observable<Category[]>;
   
-  constructor(private categoriesService: CategoriesService,
+  constructor(
     private router: Router,
-    private route: ActivatedRoute) { }
+    private route: ActivatedRoute,
+    private store: Store<AppState>) { }
 
   ngOnInit() {
-    this.categories = this.categoriesService.getList();
+    this.categories = this.store.select(x => x.core.categories).pipe(filter(x=>!!x), map(x=>Array.from(x.values())));
   }
 
   selectCategory(category: Category) {
