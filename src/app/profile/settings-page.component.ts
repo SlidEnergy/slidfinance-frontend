d78e1da0d5bc41beba8d05df031d7486afed1d4d
slidenergy <slidenergy@gmail.com>
2019-11-29 12:26:18 +1000

    chore: добавил виджет телеграма

--- src/app/profile/settings-page.component.ts
+++ src/app/profile/settings-page.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Inject, OnInit} from '@angular/core';
 import {AuthService} from '../core/auth/auth.service';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-settings-page',
   templateUrl: './settings-page.component.html',
   styleUrls: ['./settings-page.component.scss']
 })
-export class SettingsPageComponent implements OnInit {
+export class SettingsPageComponent implements OnInit, AfterViewInit {
 
-  telegramUrl = "https://telegram.me/SlidFinanceBot?start=" + AuthService.getRefreshToken();
+  telegramUrl = 'https://telegram.me/SlidFinanceBot?start=' + AuthService.getRefreshToken();
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document, private elementRef: ElementRef) {
+  }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    const s = this.document.createElement('script');
+    s.async = 'true';
+    s.src = 'https://telegram.org/js/telegram-widget.js?7';
+    s.setAttribute('data-telegram-login', 'SlidFinanceBot');
+    s.setAttribute('data-size', 'large');
+    s.setAttribute('data-auth-url', 'https://slidfinance-frontend.herokuapp.com/telegram');
+    s.setAttribute('data-request-access', 'write');
+
+    this.elementRef.nativeElement.appendChild(s);
+  }
 }
