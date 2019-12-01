d40482c47038dfbbb348c828ae4a66fd3a6510e1
slidenergy <slidenergy@gmail.com>
2019-12-02 09:21:51 +1000

    refactor: поменял виджет на неофициальный, который должен работать в России

--- src/app/profile/settings-page.component.ts
+++ src/app/profile/settings-page.component.ts
@@ -20,12 +20,18 @@ export class SettingsPageComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     const s = this.document.createElement('script');
     s.async = 'true';
-    s.src = 'https://telegram.org/js/telegram-widget.js?7';
+    //s.src = 'https://telegram.org/js/telegram-widget.js?7';
+    s.src = 'https://oauth.watg.ru/js/telegram-widget.js?5';
     s.setAttribute('data-telegram-login', 'SlidFinanceBot');
     s.setAttribute('data-size', 'large');
-    s.setAttribute('data-auth-url', 'https://slidfinance-frontend.herokuapp.com/telegram');
+    //s.setAttribute('data-auth-url', 'https://slidfinance-frontend.herokuapp.com/telegram');
+    s.setAttribute('data-onauth', 'onTelegramAuth(user)');
     s.setAttribute('data-request-access', 'write');
 
     this.elementRef.nativeElement.appendChild(s);
+  }
+
+  onTelegramAuth(user: {first_name: string, last_name: string, id: number, username: string}) {
+
   }
 }
