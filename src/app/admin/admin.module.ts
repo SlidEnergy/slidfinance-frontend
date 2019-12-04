81029f122c73eedaa62891023c98ec8510f51225
slidenergy <slidenergy@gmail.com>
2019-12-05 09:43:53 +1000

    fix: починил редактирование списка банков для админа

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,14 +4,22 @@ import {RouterModule, Routes} from '@angular/router';
 import {SharedModule} from '../shared/shared.module';
 import { UsersPageComponent } from './users/users-page.component';
 import { UserListComponent } from './users/user-list.component';
+import {BanksPageComponent} from './banks/banks-page.component';
+import {BankListComponent} from './banks/bank-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'users', pathMatch: 'full' },
   { path: 'users', component: UsersPageComponent },
+  { path: 'banks', component: BanksPageComponent },
 ];
 
 @NgModule({
-  declarations: [UsersPageComponent, UserListComponent],
+  declarations: [
+    UsersPageComponent,
+    UserListComponent,
+    BanksPageComponent,
+    BankListComponent
+  ],
   imports: [
     CommonModule,
     SharedModule,
