21377b70844f505984e1021ba9804e3037b8bbb6
slidenergy <slidenergy@gmail.com>
2019-12-30 12:12:22 +1000

    feat: добавил вывод торговых точек

--- src/app/home/merchants/merchant-list.component.ts
+++ src/app/home/merchants/merchant-list.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Merchant} from '../../api';
+import {MccService} from '../../core/mcc/mcc.service';
+import {Mcc} from '../../core/mcc/mcc';
 
 @Component({
   selector: 'app-merchant-list',
@@ -9,9 +11,25 @@ import {Merchant} from '../../api';
 export class MerchantListComponent implements OnInit {
   @Input() merchants: Merchant[];
 
-  constructor() { }
+  mcc: Mcc[];
 
-  ngOnInit() {
+  constructor(private mccService: MccService) {
   }
 
+  ngOnInit() {
+    this.mccService.getList().subscribe(x => this.mcc = x);
+  }
+
+  getMccCodeById(id: number) {
+    if (!this.mcc) {
+      return;
+    }
+
+    let mcc = this.mcc.find(x => x.id == id);
+    if (!mcc) {
+      return;
+    }
+
+    return mcc.code;
+  }
 }
