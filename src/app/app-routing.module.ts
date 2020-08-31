e5c9c716ec2d2f127c41f803785a31d638a9e7db
slidenergy <slidenergy@gmail.com>
2020-08-31 18:07:17 +1000

    refactor: обновил роутинг без использование строк

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,34 @@
 import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import {NotLoggedInGuard} from './core/not-logged-in-guard.service';
 import {AuthGuard} from './core/auth/auth-guard.service';
 import {AdminGuard} from './core/auth/admin-guard.service';
 
 const routes: Routes = [
-  {path: '', loadChildren: './home/home.module#HomeModule'},
-  {path: 'summary', loadChildren: './summary/summary.module#SummaryModule'},
-  {path: 'entry', loadChildren: './entry/entry.module#EntryModule', canActivate: [NotLoggedInGuard]},
-  {path: 'banks', loadChildren: './banks/banks.module#BanksModule', canActivate: [AuthGuard]},
-  {path: 'statictics', loadChildren: './statictics/statictics.module#StaticticsModule', canActivate: [AuthGuard]},
-  {path: 'profile', loadChildren: './profile/profile.module#ProfileModule', canActivate: [AuthGuard]},
-  {path: 'admin', loadChildren: './admin/admin.module#AdminModule', canActivate: [AdminGuard]}
+    {path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
+    {path: 'summary', loadChildren: () => import('./summary/summary.module').then(m => m.SummaryModule)},
+    {
+        path: 'entry',
+        loadChildren: () => import('./entry/entry.module').then(m => m.EntryModule),
+        canActivate: [NotLoggedInGuard]
+    },
+    {path: 'banks', loadChildren: () => import('./banks/banks.module').then(m => m.BanksModule), canActivate: [AuthGuard]},
+    {
+        path: 'statictics',
+        loadChildren: () => import('./statictics/statictics.module').then(m => m.StaticticsModule),
+        canActivate: [AuthGuard]
+    },
+    {
+        path: 'profile',
+        loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule),
+        canActivate: [AuthGuard]
+    },
+    {path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule), canActivate: [AdminGuard]}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
