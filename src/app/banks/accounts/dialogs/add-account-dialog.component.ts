0f9e9117cf5d71d733f3e82f521a30c1164b8a41
slidenergy <slidenergy@gmail.com>
2020-08-31 16:02:54 +1000

    ui: добавил необязательное поле для указания привязанного аккаунта в saltedge

--- src/app/banks/accounts/dialogs/add-account-dialog.component.ts
+++ src/app/banks/accounts/dialogs/add-account-dialog.component.ts
@@ -1,33 +1,34 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {Bank, BankAccount, BanksService} from 'src/app/api';
 import {FormControl, Validators} from '@angular/forms';
 import {map} from 'rxjs/operators';
 
 @Component({
-  selector: 'app-add-account-dialog',
-  templateUrl: './add-account-dialog.component.html',
-  styleUrls: ['./add-account-dialog.component.scss']
+    selector: 'app-add-account-dialog',
+    templateUrl: './add-account-dialog.component.html',
+    styleUrls: ['./add-account-dialog.component.scss']
 })
 export class AddAccountDialogComponent implements OnInit {
-  banks: Map<number, Bank>;
+    banks: Map<number, Bank>;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: BankAccount,
-              private banksService: BanksService) { }
+    formControl = new FormControl('', [
+        Validators.required
+    ]);
 
-  ngOnInit() {
-    this.banksService.getList().pipe(map(x => new Map(x.map(i => [i.id, i] as [number, Bank])))).subscribe(data => this.banks = data);
-  }
+    constructor(@Inject(MAT_DIALOG_DATA) public data: BankAccount,
+                private banksService: BanksService) {
+    }
 
-  formControl = new FormControl('', [
-    Validators.required
-  ]);
+    ngOnInit() {
+        this.banksService.getList().pipe(map(x => new Map(x.map(i => [i.id, i] as [number, Bank])))).subscribe(data => this.banks = data);
+    }
 
-  getErrorMessage() {
-    return this.formControl.hasError('required') ? 'Required field' : '';
-  }
+    getErrorMessage() {
+        return this.formControl.hasError('required') ? 'Required field' : '';
+    }
 
-  getBanksArray() {
-    return this.banks && Array.from(this.banks.values());
-  }
+    getBanksArray() {
+        return this.banks && Array.from(this.banks.values());
+    }
 }
--- src/app/banks/accounts/dialogs/edit-account-dialog.component.ts
+++ src/app/banks/accounts/dialogs/edit-account-dialog.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { BankAccount } from 'src/app/api';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {BankAccount, SaltedgeService} from 'src/app/api';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-edit-account-dialog',
-  templateUrl: './edit-account-dialog.component.html',
-  styleUrls: ['./edit-account-dialog.component.scss']
+    selector: 'app-edit-account-dialog',
+    templateUrl: './edit-account-dialog.component.html',
+    styleUrls: ['./edit-account-dialog.component.scss']
 })
 export class EditAccountDialogComponent implements OnInit {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: BankAccount) { }
+    formControl = new FormControl('', [
+        Validators.required
+    ]);
 
-  ngOnInit() {
-  }
+    constructor(@Inject(MAT_DIALOG_DATA) public data: BankAccount,
+                private saltedgeService: SaltedgeService) {
+    }
 
+    ngOnInit() {
+    }
+
+    getErrorMessage() {
+        return this.formControl.hasError('required') ? 'Required field' : '';
+    }
+
+    getSaltedgeBankAccountArray() {
+    }
 }
