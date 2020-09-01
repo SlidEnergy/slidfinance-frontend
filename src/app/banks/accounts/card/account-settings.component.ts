77c2107033c595b18988f0cf0ed800e0892ece0f
slidenergy <slidenergy@gmail.com>
2020-09-01 13:24:44 +1000

    refactor: перенес редактирование привязанного saltedge аккаунта в настройки

--- src/app/banks/accounts/card/account-settings.component.ts
+++ src/app/banks/accounts/card/account-settings.component.ts
@@ -1,44 +1,79 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {MessageDialogComponent} from '../../../shared/message-dialog/message-dialog.component';
+import {filter, map, switchMapTo} from 'rxjs/operators';
+import {MatDialog} from '@angular/material/dialog';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {Router} from '@angular/router';
+import {SaltedgeBankAccountRecord} from '../dialogs/SaltedgeBankAccountRecord';
+import {Observable} from 'rxjs';
+import {EntityDataContextService} from '../../../core/entity/entity-data-context.service';
 import {AccountsService, BankAccount} from '../../../api';
-import {MessageDialogComponent} from "../../../shared/message-dialog/message-dialog.component";
-import {filter, switchMapTo} from "rxjs/operators";
-import {MatDialog} from "@angular/material/dialog";
-import {MatSnackBar} from "@angular/material/snack-bar";
-import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-account-settings',
-  templateUrl: './account-settings.component.html',
-  styleUrls: ['./account-settings.component.scss']
+    selector: 'app-account-settings',
+    templateUrl: './account-settings.component.html',
+    styleUrls: ['./account-settings.component.scss']
 })
 export class AccountSettingsComponent implements OnInit {
-  @Input() account: BankAccount;
+    @Input() account: BankAccount;
+    saltedgeAccounts: Observable<SaltedgeBankAccountRecord[]>;
 
-  constructor(private dialog: MatDialog,
-              private accountsService: AccountsService,
-              private snackBar: MatSnackBar,
-              private router: Router) {
-  }
+    constructor(private dialog: MatDialog,
+                private accountsService: AccountsService,
+                private snackBar: MatSnackBar,
+                private router: Router,
+                private entityDataContext: EntityDataContextService
+    ) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.saltedgeAccounts = this.entityDataContext.saltedgeBankAccounts.getListLazy().pipe(
+            map(result => {
+                const accounts: SaltedgeBankAccountRecord[] = [];
+                for (const record of result) {
+                    for (const account of record.accounts) {
+                        accounts.push({
+                            accountId: account.id,
+                            accountName: account.name,
+                            providerName: record.connection.provider_name
+                        });
+                    }
+                }
 
-  deleteItem() {
-    const dialogRef = this.dialog.open(MessageDialogComponent, {
-      data: {caption: 'Вы уверены что хотите удалить карту?', text: this.account.title}
-    });
+                return accounts;
+            })
+        );
+    }
 
-    dialogRef.afterClosed().pipe(
-      filter(x => x),
-      switchMapTo(this.accountsService._delete(this.account.id))
-    ).subscribe(
-      data => {
-        this.snackBar.open('Карта удалена', undefined, {duration: 5000, panelClass: ['background-green']});
-        this.router.navigate(['banks']);
-      },
-      error => {
-        this.snackBar.open('Не удалось удалить карту', undefined, {duration: 5000, panelClass: ['background-red']});
-      }
-    );
-  }
+    deleteItem() {
+        const dialogRef = this.dialog.open(MessageDialogComponent, {
+            data: {caption: 'Вы уверены что хотите удалить карту?', text: this.account.title}
+        });
+
+        dialogRef.afterClosed().pipe(
+            filter(x => x),
+            switchMapTo(this.accountsService._delete(this.account.id))
+        ).subscribe(
+            data => {
+                this.snackBar.open('Карта удалена', undefined, {duration: 5000, panelClass: ['background-green']});
+                this.router.navigate(['banks']);
+            },
+            error => {
+                this.snackBar.open('Не удалось удалить карту', undefined, {duration: 5000, panelClass: ['background-red']});
+            }
+        );
+    }
+
+    saltedgeAccount_selectionChange() {
+        if (this.account.saltedgeBankAccountId) {
+            this.saveChanges();
+        }
+    }
+
+    saveChanges() {
+        this.accountsService.update(this.account.id, this.account).subscribe(
+            value => this.snackBar.open('Изменения сохранены', undefined, {duration: 5000, panelClass: ['background-green']}),
+            error => this.snackBar.open('Не удалось сохранить изменения', undefined, {duration: 5000, panelClass: ['background-red']})
+        );
+    }
 }
--- src/app/banks/accounts/dialogs/edit-account-dialog.component.ts
+++ src/app/banks/accounts/dialogs/edit-account-dialog.component.ts
@@ -2,10 +2,6 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 import {BankAccount} from 'src/app/api';
 import {FormControl, Validators} from '@angular/forms';
-import {EntityDataContextService} from '../../../core/entity/entity-data-context.service';
-import {map} from 'rxjs/operators';
-import {SaltedgeBankAccountRecord} from './SaltedgeBankAccountRecord';
-import {Observable} from 'rxjs';
 
 @Component({
     selector: 'app-edit-account-dialog',
@@ -13,38 +9,18 @@ import {Observable} from 'rxjs';
     styleUrls: ['./edit-account-dialog.component.scss']
 })
 export class EditAccountDialogComponent implements OnInit {
-    saltedgeAccounts: Observable<SaltedgeBankAccountRecord[]>;
     formControl = new FormControl('', [
         Validators.required
     ]);
 
     constructor(@Inject(MAT_DIALOG_DATA) public data: BankAccount,
-                private entityDataContext: EntityDataContextService) {
+    ) {
     }
 
     ngOnInit() {
-        this.saltedgeAccounts = this.entityDataContext.saltedgeBankAccounts.getListLazy().pipe(
-            map(result => {
-                const accounts: SaltedgeBankAccountRecord[] = [];
-                for (const record of result) {
-                    for (const account of record.accounts) {
-                        accounts.push({
-                            accountId: account.id,
-                            accountName: account.name,
-                            providerName: record.connection.provider_name
-                        });
-                    }
-                }
-
-                return accounts;
-            })
-        );
     }
 
     getErrorMessage() {
         return this.formControl.hasError('required') ? 'Required field' : '';
-    }
-
-    getSaltedgeBankAccountArray() {
     }
 }
