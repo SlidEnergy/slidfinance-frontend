ebceb4ec0a5720659d3cb7a0724c98205de709de
slidenergy <slidenergy@gmail.com>
2020-09-02 14:37:12 +1000

    ui: добавил кнопку для запуска импорта из saltedge

--- src/app/banks/accounts/account-list.component.ts
+++ src/app/banks/accounts/account-list.component.ts
@@ -1,61 +1,72 @@
-import {Component, OnInit, Input} from '@angular/core';
-import {AccountsService, BankAccount} from 'src/app/api';
+import {Component, Input, OnInit} from '@angular/core';
+import {AccountsService, BankAccount, SaltedgeService} from 'src/app/api';
 import {MatDialog} from '@angular/material/dialog';
 import {AddAccountDialogComponent} from './dialogs/add-account-dialog.component';
 import {filter, switchMap} from 'rxjs/operators';
 import {Router} from '@angular/router';
-import {MatSnackBar} from "@angular/material/snack-bar";
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
-  selector: 'app-account-list',
-  templateUrl: './account-list.component.html',
-  styleUrls: ['./account-list.component.scss']
+    selector: 'app-account-list',
+    templateUrl: './account-list.component.html',
+    styleUrls: ['./account-list.component.scss']
 })
 export class AccountListComponent implements OnInit {
-  accounts: BankAccount[];
+    accounts: BankAccount[];
 
-  @Input('accounts') set accountsInput(value: BankAccount[]) {
-    if (value) {
-      this.loadingVisible = false;
-      this.accounts = value;
+    @Input('accounts') set accountsInput(value: BankAccount[]) {
+        if (value) {
+            this.loadingVisible = false;
+            this.accounts = value;
+        }
     }
-  }
-  loadingVisible = true;
 
-  constructor(private dialog: MatDialog,
-              private router: Router,
-              private snackBar: MatSnackBar,
-              private accountsService: AccountsService) {
-  }
+    loadingVisible = true;
 
-  ngOnInit() {
-  }
+    constructor(private dialog: MatDialog,
+                private router: Router,
+                private snackBar: MatSnackBar,
+                private accountsService: AccountsService,
+                private saltedgeService: SaltedgeService) {
+    }
 
-  row_click(row: BankAccount) {
-    this.router.navigate(['banks', row.id, 'accounts']);
-  }
+    ngOnInit() {
+    }
 
-  addNew() {
-    const dialogRef = this.dialog.open(AddAccountDialogComponent, {
-      data: {}
-    });
+    row_click(row: BankAccount) {
+        this.router.navigate(['banks', row.id, 'accounts']);
+    }
 
-    dialogRef.afterClosed().pipe(
-      filter(x => x),
-      switchMap(item => this.accountsService.add(item))
-    ).subscribe(result => {
-        this.snackBar.open('Счет привязан', undefined, {duration: 5000, panelClass: ['background-green']});
-        this.accounts.push(result);
-      },
-      error => {
-        console.error(error);
-        this.snackBar.open('Не удалось привязать счет', undefined, {duration: 5000, panelClass: ['background-red']});
-      });
-  }
+    addNew() {
+        const dialogRef = this.dialog.open(AddAccountDialogComponent, {
+            data: {}
+        });
 
-  getTotalOwnFunds() {
-    if(this.accounts)
-      return this.accounts.map(b => b.balance - b.creditLimit).reduce((acc, value) => acc + value, 0);
-  }
+        dialogRef.afterClosed().pipe(
+            filter(x => x),
+            switchMap(item => this.accountsService.add(item))
+        ).subscribe(result => {
+                this.snackBar.open('Счет привязан', undefined, {duration: 5000, panelClass: ['background-green']});
+                this.accounts.push(result);
+            },
+            error => {
+                console.error(error);
+                this.snackBar.open('Не удалось привязать счет', undefined, {duration: 5000, panelClass: ['background-red']});
+            });
+    }
 
+    getTotalOwnFunds() {
+        if (this.accounts) {
+            return this.accounts.map(b => b.balance - b.creditLimit).reduce((acc, value) => acc + value, 0);
+        }
+    }
+
+    importAll() {
+        this.saltedgeService.runImport().subscribe(value => {
+            this.snackBar.open('Импорт запущен', undefined, {duration: 5000, panelClass: ['background-green']});
+        }, error => {
+            console.error(error);
+            this.snackBar.open('Не удалось запустить импорт', undefined, {duration: 5000, panelClass: ['background-red']});
+        });
+    }
 }
--- src/app/banks/accounts/accounts-page.component.ts
+++ src/app/banks/accounts/accounts-page.component.ts
@@ -1,33 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AccountsService, BankAccount} from 'src/app/api';
-import { Observable } from 'rxjs';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import {map, filter, startWith} from 'rxjs/operators';
+import {Observable} from 'rxjs';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {filter, map, startWith} from 'rxjs/operators';
 import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
-  selector: 'app-accounts-page',
-  templateUrl: './accounts-page.component.html',
-  styleUrls: ['./accounts-page.component.scss']
+    selector: 'app-accounts-page',
+    templateUrl: './accounts-page.component.html',
+    styleUrls: ['./accounts-page.component.scss']
 })
 export class AccountsPageComponent implements OnInit {
-  accounts: Observable<BankAccount[]>;
-  isAccountSelected: Observable<boolean>;
+    accounts: Observable<BankAccount[]>;
+    isAccountSelected: Observable<boolean>;
 
-  constructor(
-    private router: Router,
-    private accountService: AccountsService,
-    private snackBar: MatSnackBar
-  ) {
-    this.isAccountSelected = this.router.events.pipe(
-      filter(event => event instanceof NavigationEnd),
-      map(event => event as NavigationEnd),
-      startWith({url: this.router.url}),
-      map(event => event.url != '/banks')
-    );
-  }
+    constructor(
+        private router: Router,
+        private accountService: AccountsService,
+        private snackBar: MatSnackBar
+    ) {
+        this.isAccountSelected = this.router.events.pipe(
+            filter(event => event instanceof NavigationEnd),
+            map(event => event as NavigationEnd),
+            startWith({url: this.router.url}),
+            map(event => event.url != '/banks')
+        );
+    }
 
-  ngOnInit() {
-    this.accounts = this.accountService.getList();
-  }
+    ngOnInit() {
+        this.accounts = this.accountService.getList();
+    }
 }
